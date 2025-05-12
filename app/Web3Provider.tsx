35b28dbeac201cdd604210fd6fab5cfe377c0e18
Arjuna Marcelino <arjunamarcelino@gmail.com>
2025-05-12 18:11:24 +0700

    feat: add wallet project id

--- app/Web3Provider.tsx
+++ app/Web3Provider.tsx
@@ -6,7 +6,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { XellarKitProvider, defaultConfig, darkTheme } from "@xellar/kit";
 import { base, baseSepolia, liskSepolia } from "viem/chains";
 
-const walletConnectProjectId = "YOUR_WALLET_CONNECT_PROJECT_ID";
+const walletConnectProjectId = "306574a34ee588efaea770ec120a0060";
 const xellarAppId = "4ca5bd5d-52ff-43f0-9d19-fce2a11fd65b";
 
 const config = defaultConfig({
